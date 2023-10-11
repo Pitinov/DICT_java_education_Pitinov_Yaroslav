b1d96c9c2109e48cbac62432f856161226ebbfa3
Pitinov <y.p.pitinov@student.khai.edu>
2023-10-11 23:47:13 +0300

    added one more println in Test.java at the feature_branch

--- src/Test.java
+++ src/Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("education");
         //comment on second string of the func
+        System.out.println("DICT");
     }
 }
