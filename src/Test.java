1edd16d2c61d11879148cdf24c26d7cf7b65a878
Pitinov <y.p.pitinov@student.khai.edu>
2023-10-12 00:08:53 +0300

    Resolved conflict

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,5 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("education");
-        //comment on second string of the func
-        System.out.println("DICT");
+        System.out.println("Javaeducation");
     }
 }
