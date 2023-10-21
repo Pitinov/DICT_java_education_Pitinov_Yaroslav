b71635e0ceb8f6ae5d5c2a790d3dcb19cab28e5e
Pitinov <y.p.pitinov@student.khai.edu>
2023-10-21 14:57:04 +0300

    3st stage

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,9 +1,13 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class Hangman {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String secretWord = "java";
+        Random random = new Random();
+
+        String[] words = {"python", "java", "javascript", "kotlin"};
+        String secretWord = words[random.nextInt(words.length)]; // Випадковий вибір слова зі списку
 
         System.out.println("HANGMAN");
         System.out.print("Guess the word: > ");
