fd7d6a321b6a988f891bef104b25cc71723f72b7
Pitinov <y.p.pitinov@student.khai.edu>
2023-10-21 14:54:48 +0300

    2st stage

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 public class Hangman {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String secretWord = "java";
+
         System.out.println("HANGMAN");
-        System.out.println("The game will be available soon.");
+        System.out.print("Guess the word: > ");
+
+        String userGuess = scanner.next();
+
+        if (userGuess.equalsIgnoreCase(secretWord)) {
+            System.out.println("You survived!");
+        } else {
+            System.out.println("You lost!");
+        }
     }
 }
-
