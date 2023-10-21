4160a7ab217339d473c6b97433850891363a625b
Pitinov <y.p.pitinov@student.khai.edu>
2023-10-21 15:20:25 +0300

    5st stage

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -8,16 +8,45 @@ public class Hangman {
 
         String[] words = {"python", "java", "javascript", "kotlin"};
         String secretWord = words[random.nextInt(words.length)]; // Випадковий вибір слова зі списку
+        StringBuilder guessedWord = new StringBuilder(secretWord.length());
+
+        for (int i = 0; i < secretWord.length(); i++) {
+            guessedWord.append("-");
+        }
+
+        int remainingAttempts = 8;
 
         System.out.println("HANGMAN");
-        System.out.print("Guess the word: > ");
+        System.out.println(guessedWord);
 
-        String userGuess = scanner.next();
+        while (remainingAttempts > 0) {
+            System.out.print("Input a letter: > ");
+            char inputLetter = scanner.next().charAt(0);
+            boolean letterFound = false;
 
-        if (userGuess.equalsIgnoreCase(secretWord)) {
-            System.out.println("You survived!");
-        } else {
-            System.out.println("You lost!");
+            for (int i = 0; i < secretWord.length(); i++) {
+                if (secretWord.charAt(i) == inputLetter && guessedWord.charAt(i) == '-') {
+                    guessedWord.setCharAt(i, inputLetter);
+                    letterFound = true;
+                }
+            }
+
+            if (letterFound) {
+                System.out.println(guessedWord);
+            } else {
+                System.out.println("That letter doesn't appear in the word");
+                remainingAttempts--;
+            }
+
+            if (guessedWord.toString().equals(secretWord)) {
+                System.out.println("Thanks for playing!");
+                System.out.println("We'll see how well you did in the next stage");
+                break;
+            }
+        }
+
+        if (remainingAttempts == 0) {
+            System.out.println("You ran out of attempts. The word was: " + secretWord);
         }
     }
 }
