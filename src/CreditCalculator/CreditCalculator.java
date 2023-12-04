e4fd76b33757d2194993e3a64bfd93f4cd0e7c42
Pitinov <y.p.pitinov@student.khai.edu>
2023-12-04 12:27:59 +0200

    CreditCalculator 2

--- src/CreditCalculator/CreditCalculator.java
+++ src/CreditCalculator/CreditCalculator.java
@@ -9,30 +9,35 @@ public class CreditCalculator {
         System.out.print("Enter the loan principal: ");
         double principal = scanner.nextDouble();
 
-        System.out.print("Enter the annual interest rate (as a percentage): ");
-        double annualInterestRate = scanner.nextDouble();
+        System.out.println("What do you want to calculate?");
+        System.out.println("Type \"m\" – for the number of monthly payments,");
+        System.out.println("Type \"p\" – for the monthly payment:");
+        char calculationType = scanner.next().charAt(0);
 
-        double monthlyInterestRate = annualInterestRate / 100 / 12;
-        System.out.print("Enter the number of months: ");
-        int numberOfMonths = scanner.nextInt();
-
-        double monthlyPayment = calculateMonthlyPayment(principal, monthlyInterestRate, numberOfMonths);
-
-        for (int month = 1; month <= numberOfMonths; month++) {
-            double monthlyRepayment = Math.min(monthlyPayment, principal);
-            principal -= monthlyRepayment;
-
-            System.out.println("Month " + month + ": repaid " + monthlyRepayment);
-
-            if (principal <= 0) {
-                System.out.println("The loan has been repaid!");
-                break;
-            }
+        if (calculationType == 'm') {
+            System.out.print("Enter the monthly payment: ");
+            double monthlyPayment = scanner.nextDouble();
+            int numberOfMonths = calculateNumberOfMonths(principal, monthlyPayment);
+            System.out.println("It will take " + numberOfMonths + " months to repay the loan");
+        } else if (calculationType == 'p') {
+            System.out.print("Enter the number of months: ");
+            int numberOfMonths = scanner.nextInt();
+            double monthlyPayment = calculateMonthlyPayment(principal, numberOfMonths);
+            double lastPayment = calculateLastPayment(principal, numberOfMonths, monthlyPayment);
+            System.out.println("Your monthly payment = " + (int) Math.ceil(monthlyPayment) +
+                    " and the last payment = " + (int) Math.floor(lastPayment));
         }
     }
 
-    private static double calculateMonthlyPayment(double principal, double monthlyInterestRate, int numberOfMonths) {
-        return principal * (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, numberOfMonths))
-                / (Math.pow(1 + monthlyInterestRate, numberOfMonths) - 1);
+    private static int calculateNumberOfMonths(double principal, double monthlyPayment) {
+        return (int) Math.ceil(principal / monthlyPayment);
+    }
+
+    private static double calculateMonthlyPayment(double principal, int numberOfMonths) {
+        return principal / numberOfMonths;
+    }
+
+    private static double calculateLastPayment(double principal, int numberOfMonths, double monthlyPayment) {
+        return principal - (numberOfMonths - 1) * monthlyPayment;
     }
 }
